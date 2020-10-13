efb883c935b23fc4c9885ee2bf6163ac808c33a1
tompori <tom.nuorivaara@kolumbus.fi>
2020-10-13 19:02:10 +0300

    Started implementing User model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+// Interface that describes the properties to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// Interface that describes the properties that a User model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>("User", userSchema);
+
+export { User };
