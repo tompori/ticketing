48dd7a3ed0ab8f6a2a65b7482aebd2c474f2645a
tompori <tom.nuorivaara@kolumbus.fi>
2020-10-25 15:29:05 +0200

    Implemented redirect on successful signup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
 const SignUp = () => {
@@ -9,6 +9,7 @@ const SignUp = () => {
     url: "/api/users/signup",
     method: "post",
     body: { email, password },
+    onSuccess: () => Router.push("/"),
   });
 
   const onSubmit = async (event) => {
