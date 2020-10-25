3a7df8c329f8a6157a2c6ac459f381550ba97933
tompori <tom.nuorivaara@kolumbus.fi>
2020-10-25 15:08:26 +0200

    Implemented signup request and error handling in client

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,13 +1,24 @@
 import { useState } from "react";
+import axios from "axios";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
 
-    console.log(email, password);
+    try {
+      const response = await axios.post("/api/users/signup", {
+        email,
+        password,
+      });
+      setErrors([]);
+      console.log(response.data);
+    } catch (err) {
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
@@ -31,6 +42,16 @@ const SignUp = () => {
           className="form-control"
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Oops...</h4>
+          <ul className="my-0">
+            {errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
