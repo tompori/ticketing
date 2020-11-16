a26f0bc7ff527acb93699ac70f8e4a9da1497481
tompori <tom.nuorivaara@kolumbus.fi>
2020-11-16 10:49:50 +0200

    Refactored to using common npm package

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,8 +2,7 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 import { Password } from "../services/password";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest, BadRequestError } from "@tomporiticketstest/common";
 import { User } from "../models/user";
 
 const router = express.Router();
