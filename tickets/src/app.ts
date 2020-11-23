7ab9a16c01442189fe9958d00695cf48fdeee2f8
tompori <tom.nuorivaara@kolumbus.fi>
2020-11-23 13:21:42 +0200

    Implemented show ticket route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -8,6 +8,7 @@ import {
   currentUser,
 } from "@tomporiticketstest/common";
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
 
 const app = express();
 app.set("trust proxy", true);
@@ -21,6 +22,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
