3b9423a9ba613d0fd80c46eb5319a1cfb68eec74
tompori <tom.nuorivaara@kolumbus.fi>
2020-11-27 17:06:10 +0200

    Wrote tests for events

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns a 404 if the id does not exist", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -107,4 +108,30 @@ it("updates the ticket provided valid inputs", async () => {
 
   expect(ticketResponse.body.title).toEqual(updatedTitle);
   expect(ticketResponse.body.price).toEqual(updatedPrice);
+});
+
+it("publishes an event", async () => {
+  const cookie = global.signup();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "Test ticket",
+      price: 20,
+    });
+
+  const updatedTitle = "New title";
+  const updatedPrice = 100;
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: updatedTitle,
+      price: updatedPrice,
+    })
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
