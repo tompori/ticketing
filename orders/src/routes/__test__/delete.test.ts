8cc16b575f05f736533e9584c2c1213a8408c6e2
tompori <tom.nuorivaara@kolumbus.fi>
2020-12-01 14:54:24 +0200

    Implemented cancel order

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,35 @@
+import { body } from "express-validator";
+import request from "supertest";
+import { app } from "../../app";
+import { Order, OrderStatus } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+
+it("marks an order as cancelled", async () => {
+  // Create a ticket with Ticket Model
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signup();
+  // Make a request to create an order
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  // Make sure the order is cancelled
+  const updatedOrder = await Order.findById(order.id);
+  expect(updatedOrder?.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo("emits an order updated event");
