b18b40e2de62458ba6fb3a63357e4486012dcd0c
tompori <tom.nuorivaara@kolumbus.fi>
2020-12-02 15:16:34 +0200

    Implemented event publishing for new order and delete order

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import request from "supertest";
 import { app } from "../../app";
 import { Order, OrderStatus } from "../../models/order";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("marks an order as cancelled", async () => {
   // Create a ticket with Ticket Model
@@ -32,4 +33,28 @@ it("marks an order as cancelled", async () => {
   expect(updatedOrder?.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("emits an order updated event");
+it("emits an order updated event", async () => {
+  // Create a ticket with Ticket Model
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signup();
+  // Make a request to create an order
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // Make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
