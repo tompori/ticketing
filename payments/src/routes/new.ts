e17e2d6c59e3cfbc0548d7681cbb312f6c582440
tompori <tom.nuorivaara@kolumbus.fi>
2020-12-07 16:30:29 +0200

    Implemented creating a stripe client

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   OrderStatus,
 } from "@tomporiticketstest/common";
 import { Order } from "../models/order";
+import { stripe } from "../stripe";
 
 const router = express.Router();
 
@@ -31,6 +32,12 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError("Cannot pay for a cancelled order");
     }
+
+    await stripe.charges.create({
+      currency: "usd",
+      amount: order.price * 100,
+      source: token,
+    });
 
     res.send({ success: true });
   }
