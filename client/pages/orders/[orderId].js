520d882f4f3cd3157d8df55bbfeba80cea2f14aa
tompori <tom.nuorivaara@kolumbus.fi>
2020-12-08 20:01:45 +0200

    Implemented My Orders list

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Router from "next/router";
 import { DateTime } from "luxon";
 import StripeCheckout from "react-stripe-checkout";
 import useRequest from "../../hooks/use-request";
@@ -11,7 +12,7 @@ const OrderShow = ({ order, currentUser }) => {
     body: {
       orderId: order.id,
     },
-    onSuccess: (payment) => console.log(payment),
+    onSuccess: () => Router.push("/orders"),
   });
 
   useEffect(() => {
